83b00de1e51c51116bca6af47f7275ce61660436
Lavender <susie204@naver.com>
2024-07-19 01:07:37 +0900

    Initial Commit

---
+++ LottieTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieTest
+//
+//  Created by 이승현 on 7/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
